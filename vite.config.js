9093fba04b2189f203148a73c974b9934edece4a
Erfan-Programmer <erfankazemy1380@gmail.com>
2024-12-13 01:20:06 +0330

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/jodowebapp/",
   plugins: [react(), ghPages()],
+  base: "/jodowebapp/",
   server: { port: 3000 },
   build: {
     rollupOptions: {
