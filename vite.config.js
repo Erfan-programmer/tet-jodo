76f303e8c3c7b1541ba5bc10569d08f48d53d41c
Erfan-Programmer <erfankazemy1380@gmail.com>
2024-12-13 02:13:18 +0330

    tst

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), ghPages()],
-  base: "/tetodo/",
+  base: "/tet-jodo/",
   server: { port: 3000 },
   build: {
     rollupOptions: {
