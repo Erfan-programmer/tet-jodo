d78c0d74f6727b6b9b44e1a86e59c281f6e2d7e8
Erfan-Programmer <erfankazemy1380@gmail.com>
2024-12-13 01:46:59 +0330

    solved

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), ghPages()],
-  base: "/jodowebapp/",
+  base: "/tet-jodo/",
   server: { port: 3000 },
   build: {
     rollupOptions: {
